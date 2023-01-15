6db7f552477390d46bcb18f5148b747bbf54f5aa
Prince kumarE <pk41330@gmail.com>
2023-01-16 00:31:04 +0530

    added changed file

--- shopping/src/Pages/Product/Product.jsx
+++ shopping/src/Pages/Product/Product.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import "./Product.scss";
+const image = [
+  "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F49%2F1e%2F491eacb95ad3db977d5a5de5e84eef58be3c7def.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]",
+  "https://lp2.hm.com/hmgoepprod?set=format%5Bwebp%5D%2Cquality%5B79%5D%2Csource%5B%2F50%2F65%2F5065faef1d35bafd81b400d3feebefd53880fe27.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url%5Bfile%3A%2Fproduct%2Fmain%5D",
+];
 const Product = () => {
+  const [viewImage, setViewImage] = useState(0);
+  console.log(viewImage);
   return (
-    <div>Product</div>
-  )
-}
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img
+            src={image[0]}
+            onClick={() => {
+              setViewImage(0);
+            }}
+            alt=""
+          />
+          <img
+            src={image[1]}
+            onClick={() => {
+              setViewImage(1);
+            }}
+            alt=""
+          />
+        </div>
+        <div className="mainImg">
+          <img src={image[viewImage]} alt="" />
+        </div>
+      </div>
+      <div className="right"></div>
+    </div>
+  );
+};
 
-export default Product
+export default Product;
--- shopping/src/Pages/Products/Products.jsx
+++ shopping/src/Pages/Products/Products.jsx
@@ -1,7 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Products.scss";
+import List from "../../Components/List/List";
+import { useParams } from "react-router-dom";
 const Products = () => {
-  return <div></div>;
+  const [maxPrice, setMaxPrice] = useState(1500);
+  const [sort, setSort] = useState("");
+  const CategoryParamId = parseInt(useParams().id);
+  const changePriceRange = (e) => {
+    setMaxPrice(e.target.value);
+  };
+  const changeSort = (e) => {
+    setSort(e);
+  };
+  return (
+    <div className="products">
+      <div className="left">
+        <div className="fliterItem">
+          <h2>Product Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id="1" value={1} />
+            <label for="1">Shoe</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="2" value={2} />
+            <label for="2">Hoodie</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id="3" value={3} />
+            <label for="3">Skirt</label>
+          </div>
+        </div>
+        <div className="fliterItem">
+          <h2>Filter By Price</h2>
+          <div className="inputItem">
+            <span>500</span>
+            <input
+              type="range"
+              min={500}
+              max={10000}
+              onChange={(e) => {
+                changePriceRange(e);
+              }}
+            />
+            <span>{maxPrice}</span>
+          </div>
+        </div>
+        <div className="fliterItem">
+          <h2>Sort By</h2>
+          <div className="inputItem">
+            <input
+              type="radio"
+              id="asc"
+              value="asc"
+              name="price"
+              onChange={(e) => {
+                changeSort(e.target.value);
+              }}
+            />
+            <lable for="asc">Price(Low to High)</lable>
+          </div>
+          <div className="inputItem">
+            <input
+              type="radio"
+              id="des"
+              value="des"
+              name="price"
+              onChange={(e) => {
+                changeSort(e.target.value);
+              }}
+            />
+            <lable for="des">Price(High to Low)</lable>
+          </div>
+        </div>
+      </div>
+      <div className="right">
+        <img className="catImg"
+          src="https://adn-static1.nykaa.com/nykdesignstudio-images/pub/media/fermion/images/category_slider/img/tr:w-3806,/Indianwear_Teen%20Store_Desktop%20copy.jpg?rnd=20200526195200"
+          alt=""
+        />
+
+       <div className="listContainer">
+       <List
+          CategoryParamId={CategoryParamId}
+          sort={sort}
+          maxPrice={maxPrice}
+        />
+       </div>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
